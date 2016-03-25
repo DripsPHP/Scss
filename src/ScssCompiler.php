cb3afc144d2b4c9a7778f9cb122f52c779f30aa0
sorrina <1981maariaa@gmail.com>
2016-03-25 20:41:20 +0100

    Scss CompilerTests

--- src/ScssCompiler.php
+++ src/ScssCompiler.php
@@ -3,10 +3,12 @@
 namespace Drips\SccCompiler;
 
 use Drips\Utils\ICompiler;
+use scssc;
 
 class ScssCompiler implements ICompiler
 {
     public static function compile($string){
-
+        $scss = new scssc;
+        return $scss->compile($string);
     }
 }
