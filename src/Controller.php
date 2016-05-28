a786f67d1b117dd8d45d39e42926dbc0d07c4ad2
Raffael Kessler <raffael.kessler@prowect.com>
2016-05-28 15:44:29 +0200

    MinimizeCompiler hinzugefügt

--- src/Controller.php
+++ src/Controller.php
@@ -3,17 +3,23 @@
 namespace Drips\ScssCompiler;
 
 use Drips\MVC\CompileController;
+use Drips\MinimizeCompiler\MinimizeCompiler;
 
 class Controller extends CompileController
 {
     protected $source_directory = "scss";
     protected $file_extension = "scss";
     protected $response_type = "text/css";
+    protected $minimize = true;
 
     public function compile($content)
     {
         $compiler = new ScssCompiler;
         $compiler->setImportPaths($this->source_directory);
-        return $compiler->compile($content);
+        $compiled = $compiler->compile($content);
+        if($this->minimize){
+            $compiled = MinimizeCompiler::compile($compiled, MinimizeCompiler::CSS);
+        }
+        return $compiled;
     }
 }
