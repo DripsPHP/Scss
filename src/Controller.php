3661735bfecf15ac4f7e0eed1e464f91142e45ec
Raffael Kessler <raffael.kessler@prowect.com>
2016-06-24 22:00:29 +0200

    PHP 5.4 Compatibility

--- src/Controller.php
+++ src/Controller.php
@@ -5,9 +5,11 @@ namespace Drips\ScssCompiler;
 use Drips\MVC\CompileController;
 use Drips\MinimizeCompiler\MinimizeCompiler;
 
+define('DRIPS_SCSS_DIRECTORY', DRIPS_DIRECTORY.'/scss');
+
 class Controller extends CompileController
 {
-    protected $source_directory = DRIPS_DIRECTORY."/scss";
+    protected $source_directory = DRIPS_SCSS_DIRECTORY;
     protected $file_extension = "scss";
     protected $response_type = "text/css";
     protected $minimize = true;
